0234965d0134ecb74d8a97ca055ccf119be00ebe
josuerojasrojas <josuerojas.rojas@gmail.com>
2022-11-30 20:41:23 -0500

    update text

--- src/presents.ts
+++ src/presents.ts
@@ -3,43 +3,136 @@ import { Present } from './App';
 export const presents: {[key: string]: Present} = {
   'DECEMBER012022': {
     title: 'Birthday Month Start!',
-    note: 'Wanted to give you something special for the 1st birthday of money we celebrate together. An advent calendar seemed appropriate. A pack of M&Ms came second. Ha. Only cause it reminds me of our second date. I took note of your prefer snacks on our first date and from then on I wanted to know more about you. The calendar is to make it extra special. Hopefully in the future I can afford to give you an advent calendar with just jewelry. For now it\'s one full of mixed things. Happy birthday eve.',
+    note: `Wanted to give you something special for the 1st birthday we celebrate together. An advent calendar seemed appropriate. A pack of M&Ms came second. Ha! 😛. Only cause it reminds me of our second date. I took mental notes of your prefer snacks on our first date and from then on I wanted to know more about you. The calendar is to make it extra special and to shower you with small gifts. Hopefully in the future I can afford to give you an advent calendar with just jewelry. For now it's one full of mixed things. Happy birthday eve. I hope you enjoy the tiny presents I got you.P.S try not to cheat by going to this site before I give you your present unless you are really curious. `
   },
   'DECEMBER022022': {
-    title: 'TODO',
-    note: '',
+    title: 'HAPPY BIRTHDAY!!!',
+    note: `Sag season! Happy birthday my love! I am so happy I got to meet you and became part of your life as you are part of mine. I am so happy I get to celebrate your birthday, cause you are the greatest! 
+    Although today is your special day I rather celebrate this first birthday with you by celebrating all month long with presents. 
+    I think this idea was kinda ambitious for me but worth it! (it was last minute when we saw the advent calendars in marshalls). But it will be worth it. 
+    I love you so much! I will always try to make you happy and spoil you as much as I can.
+    Today’s gifts: Balloons, Cake, Roses, Book, and a song. `,
     emoji: '♐'
   },
   'DECEMBER032022': {
-    title: 'TODO',
-    note: '',
+    title: 'PTV!',
+    note: 'Saw this one your list and thought maybe I should get a paramour shirt for your concert. But that would involve me choosing a shirt and this was chosen already. 😛. I also thought this shirt look cool so I can potentially use it one day. Haha. ',
   },
   'DECEMBER042022': {
-    title: 'TODO',
-    note: '',
+    title: '💞 HAPPY ANNIVERSARY! 💞',
+    note: 'I thought a bit the gift to get. I should really be saving for another ring. But in the meantime I wanna give you other jewelries while I can. I+J cause I comes before J and cause they are next to each other just like I want to be with you forever. I love you very much. I will always do. Happy Anniversary of when we first went out. I’m so happy I get to hug you if you are cold now cause I sure really wanted to when we were up and outside in that tall building. I enjoyed all the conversations we had that day. I knew from there you were different than me. But complimented me. I really appreciate all your opinions. To the smartest and most beautiful girl, my love! Happy anniversary!',
+    emoji: '❤️'
   },
   'DECEMBER052022': {
-    title: 'TODO',
-    note: '',
+    title: 'Lets build a house! 🛖',
+    note: 'A ginger bread house. It’s part of our list. So not a big surprise. Just like the pictures you showed me, let’s make a ginger bread house together this time. With your expertise and my immigrant instincts I think we can do a decent job. ',
   },
   'DECEMBER062022': {
-    title: 'TODO',
-    note: '',
+    title: 'Slinky!',
+    note: 'I didn’t find the same slinky you gave me. But I figure this junior one was as good to fidget. I got it so you won’t take the one you gave me. Haha. Also story: I got this in beacon. When we got mad. I really wanted to hug you. I offered you my shirt when you were cold but you didn’t want it. Thats when I thought I can use that moment as an opportunity to look for a slinky. I felt really terrible for not being next to you in that toy store. So I also got you m&ms. I’m sorry for making you mad. I understand we were both hungry. I do love you very much. Always. And I always appreciate you for teaching me to talk through things. That is why our relationship is great. We talk through things. We solve stuff. And one things is always constant. The love. I will always love you. Even when I might get mad. I always remember that. I love you very much!',
   },
   'DECEMBER072022': {
-    title: 'TODO',
-    note: '',
+    title: 'Fish Day!',
+    note: 'Sometimes I hear you tell me you want swedish fish. lol. I figure today can be a fish day for your gift.',
+    emoji: '🐟'
   },
   'DECEMBER082022': {
-    title: 'TODO',
-    note: '',
+    title: 'Picture Day! 📸',
+    note: 'hmm. maybe the title is misleading. or not…. but today’s gift is film for your camera so we can take more cute pics together. and I can have more pics of you. one for each bag or pants I carry. When I met you I wasn’t really into being in pictures. I still am not. But I find it fun when you are in it with me. It’s a way to look back and smile through the memories we make. ',
   },
   'DECEMBER092022': {
-    title: 'TODO',
-    note: '',
+    title: 'SEEDS!',
+    note: 'I take notes sometimes but i figure this one is easier to remember that you like these seeds. Haha. Only one bag just cause I hear your stories and don’t want you to have tummy pains or troubles.',
   },
   'DECEMBER102022': {
-    title: 'TODO',
-    note: '',
+    title: 'Animal!',
+    note: 'Gonna get you a whole farm! Maybe one day we can have our farm. Full of chickens. But first we gotta figure out how to enjoy the benefits of it without work. Hint hint we become rich. For now gonna get you a glass farm. ',
+    emoji: '🥚'
   },
+  'DECEMBER112022': {
+    title: 'Charm!',
+    note: 'I wanted to diverse your jewelry collection. With a charm I figure you have more choices to wear your chains. Maybe I’ll get you a chain in the future. Wink wink (idea). Part1. Emoji is a ⭐ cause its the 11 so make a wish (I guess at 11:11 idk how it works..)',
+    emoji: '⭐'
+  },
+  'DECEMBER122022': {
+    title: 'Sour! Patch!',
+    note: 'hmmm. another candy day. in my mind I give you candy so you can share 😛. ',
+  },
+  'DECEMBER132022': {
+    title: 'Skirt Skirt!!',
+    note: 'No it’s not a car or a motorcycle. I thought it would be fair to also include things from your list. Selfishly I like skirts on you. I like anything on you. But you get what I am saying. Here’s to lots of cute outfits!',
+  },
+  'DECEMBER142022': {
+    title: 'Oh the places you’ll go!',
+    note: 'I believe there is so much potential for you. You are one of the smartest person I know. You help me clear my head sometimes. I appreciate that. Oh the place you’ll go, I’ll always support those places wherever or whatever those are. Always on your side to support you in whatever way I can. ',
+  },
+  'DECEMBER152022': {
+    title: 'Stinky day!',
+    note: 'lol. I do really enjoy how you smell, all the smells. This is obviously from your list. I could tell you a funny story about this. Just remind me. Not really funny but a story nonetheless.',
+  },
+  'DECEMBER162022': {
+    title: 'GUM!!',
+    note: 'When you told me your dad buys you gum sometimes it made me realized I have high standards to meet. Haha. That’s ok. I will try my best to spoil as much as I can. Even if it’s small like getting you gum here and there. Or small snacks. ',
+  },
+  'DECEMBER172022': {
+    title: 'Chicken #2',
+    note: 'You saw these chickens before. So not really a surprise. But you didn’t see the third animal. So stay tune.',
+    emoji: '🐓'
+  },
+  'DECEMBER182022': {
+    title: 'For your mom',
+    note: 'This is more for your mom. When I met you I realized how much you cared and love your mom from the first and second date. I’m not religious but sometimes I ask for stuff. I’m not sure if I told you but when we first started going out. I really cared about you. I figure I wanted to do something. I prayed. Even if I didn’t know. I asked to keep your mom safe. I also prayed that I wanted you to not be sad. I never knew what to say to you. But I knew I wanted to always be there for you. I care and love you so much. I will always want to be by your side. If I could I would always make you happy. I’ll always try my best to do so.',
+  },
+  'DECEMBER192022': {
+    title: 'P!',
+    note: 'Got you another charm. I realized you write “PAL” so maybe next year I’ll get A and L. Wanted to dedicate some days for your mom. Sorry this one isn’t long I figure I would be writing what I wrote yesterday. I’ll always be by your side even if I have no words, I am here for anything you might need.',
+  },
+  'DECEMBER202022': {
+    title: 'TODo',
+    note: 'TODO',
+  },
+  'DECEMBER212022': {
+    title: 'TODo',
+    note: 'TODO',
+  },
+  'DECEMBER222022': {
+    title: 'TODo',
+    note: 'TODO',
+  },
+  'DECEMBER232022': {
+    title: 'TODo',
+    note: 'TODO',
+  },
+  'DECEMBER242022': {
+    title: 'TODo',
+    note: 'TODO',
+  },
+  'DECEMBER252022': {
+    title: 'TODo',
+    note: 'TODO',
+  },
+  'DECEMBER262022': {
+    title: 'TODo',
+    note: 'TODO',
+  },
+  'DECEMBER272022': {
+    title: 'TODo',
+    note: 'TODO',
+  },
+  'DECEMBER282022': {
+    title: 'TODo',
+    note: 'TODO',
+  },
+  'DECEMBER292022': {
+    title: 'TODo',
+    note: 'TODO',
+  },
+  'DECEMBER302022': {
+    title: 'TODo',
+    note: 'TODO',
+  },
+  'DECEMBER312022': {
+    title: 'TODo',
+    note: 'TODO',
+  }
 }
